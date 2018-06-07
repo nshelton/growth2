42905a428919eb508bd55dd450816bd0409c0c81
nshelton <nshelton@gmail.com>
2018-06-07 00:25:23 -0500

    first commit

---
+++ Assets/NoiseBall2.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class NoiseBall2 : MonoBehaviour
+{
+    #region structs
+
+    private struct Edge
+    {
+        int a;
+        int b;
+    }
+
+    #endregion
+
+    #region Exposed attributes
+
+    [SerializeField] int _maxNodes = 1000;
+
+    [SerializeField] int _numNodes = 10;
+
+    [SerializeField] float _scale = 0.1f;
+    [SerializeField] float _growthRate = 0.01f;
+    [SerializeField] float _maxSize = 0.5f;
+
+    [SerializeField] Material _material;
+    [SerializeField] Mesh _mesh;
+
+    #endregion
+
+    #region Hidden attributes
+
+    [SerializeField, HideInInspector] ComputeShader _compute;
+
+    #endregion
+
+    #region Private fields
+
+    ComputeBuffer _drawArgsBuffer;
+    ComputeBuffer _nodePositionsRead, _nodePositionsWrite;
+    ComputeBuffer _edgeListBuffer;
+    MaterialPropertyBlock _props;
+
+    private int _numEdges = 1;
+
+    #endregion
+
+    #region Compute configurations
+
+    const int kThreadCount = 64;
+    int ThreadGroupCount { get { return _maxNodes / kThreadCount; } }
+    int NodeCount { get { return kThreadCount * ThreadGroupCount; } }
+    int MaxEdges { get { return NodeCount; } }
+
+    #endregion
+
+    #region MonoBehaviour functions
+
+    private void OnValidate()
+    {
+        _maxNodes = Mathf.Max(_maxNodes, kThreadCount);
+        _numNodes = Mathf.Max(_numNodes, 2);
+    }
+
+    protected void SwapBuffer(ref ComputeBuffer buf0, ref ComputeBuffer buf1)
+    {
+        var tmp = buf0;
+        buf0 = buf1;
+        buf1 = tmp;
+    }
+
+    void Init()
+    {
+        Debug.Log("Init");
+        if (_nodePositionsRead != null) _nodePositionsRead.Release();
+        if (_nodePositionsWrite != null) _nodePositionsWrite.Release();
+        if (_edgeListBuffer != null) _edgeListBuffer.Release();
+        if (_drawArgsBuffer != null) _drawArgsBuffer.Release();
+
+        _nodePositionsRead = new ComputeBuffer(_maxNodes, Marshal.SizeOf(typeof(Vector4)));
+        _nodePositionsWrite = new ComputeBuffer(_maxNodes, Marshal.SizeOf(typeof(Vector4)));
+        _edgeListBuffer = new ComputeBuffer(_maxNodes, Marshal.SizeOf(typeof(Edge)));
+
+        _drawArgsBuffer = new ComputeBuffer(
+            1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments
+        );
+
+        _numNodes = 2;
+        _numEdges = 1;
+
+        _drawArgsBuffer.SetData(new uint[5] {
+            _mesh.GetIndexCount(0),
+            (uint)NodeCount,
+            0, 0, 0});
+
+        // This property block is used only for avoiding a bug (issue #913828)
+        _props = new MaterialPropertyBlock();
+        _props.SetFloat("_UniqueID", Random.value);
+
+        var initKernel = _compute.FindKernel("InitNodes");
+        _compute.SetBuffer(initKernel, "NodeListWrite", _nodePositionsWrite);
+        _compute.Dispatch(initKernel, ThreadGroupCount, 1, 1);
+        // SwapBuffer(ref _nodePositionsRead, ref _nodePositionsWrite);
+
+    }
+
+    void OnDestroy()
+    {
+        _drawArgsBuffer.Release();
+        if (_nodePositionsRead != null) _nodePositionsRead.Release();
+        if (_nodePositionsWrite != null) _nodePositionsWrite.Release();
+        if (_edgeListBuffer != null) _edgeListBuffer.Release();
+    }
+
+    void UpdateUniforms()
+    {
+        _compute.SetFloat("Time", Time.time);
+        _compute.SetFloat("Scale", _scale);
+        _compute.SetFloat("GrowthRate", _growthRate);
+        _compute.SetFloat("MaxSize", _maxSize);
+        _compute.SetInt("NumPoints", NodeCount);
+        _compute.SetInt("NumEdges", _numEdges);
+    }
+
+    void Update()
+    {
+
+        if (_nodePositionsRead == null || _nodePositionsRead.count != NodeCount)
+        {
+            Init();
+        }
+
+        // Invoke the update compute kernel.
+        var kernel = _compute.FindKernel("UpdateNodes");
+        UpdateUniforms();
+        _compute.SetBuffer(kernel, "NodeListRead", _nodePositionsRead);
+        _compute.SetBuffer(kernel, "NodeListWrite", _nodePositionsWrite);
+        _compute.Dispatch(kernel, ThreadGroupCount, 1, 1);
+
+        
+
+
+        SwapBuffer(ref _nodePositionsRead, ref _nodePositionsWrite);
+
+        Render();
+    }
+
+    void Render()
+    {
+        _material.SetMatrix("_LocalToWorld", transform.localToWorldMatrix);
+        _material.SetMatrix("_WorldToLocal", transform.worldToLocalMatrix);
+
+        _material.SetBuffer("_NodeList", _nodePositionsRead);
+        _material.SetFloat("_Scale", _scale);
+
+        Graphics.DrawMeshInstancedIndirect(
+            _mesh, 0, _material,
+            new Bounds(transform.position, transform.lossyScale * 5),
+            _drawArgsBuffer, 0, _props, 
+            UnityEngine.Rendering.ShadowCastingMode.On, true
+        );
+
+    }
+    #endregion
+}
---
+++ Assets/NoiseBall2.shader
@@ -0,0 +1,94 @@
+﻿Shader "Custom/NoiseBall2"
+{
+    Properties
+    {
+        _Color1("Color1", Color) = (1, 1, 1, 1)
+        _Color2("Color2", Color) = (1, 1, 1, 1)
+        _Smoothness("Smoothness", Range(0, 1)) = 0
+        _Metallic("Metallic", Range(0, 1)) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Cull Off
+
+        CGPROGRAM
+
+        #pragma surface surf Standard vertex:vert addshadow
+        #pragma instancing_options procedural:setup
+        #pragma target 3.5
+
+        #if SHADER_TARGET >= 35 && (defined(SHADER_API_D3D11) || defined(SHADER_API_GLES3) || defined(SHADER_API_GLCORE) || defined(SHADER_API_XBOXONE) || defined(SHADER_API_PSSL) || defined(SHADER_API_SWITCH) || defined(SHADER_API_VULKAN) || (defined(SHADER_API_METAL) && defined(UNITY_COMPILER_HLSLCC)))
+            #define SUPPORT_STRUCTUREDBUFFER
+        #endif
+
+        #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED) && defined(SUPPORT_STRUCTUREDBUFFER)
+            #define ENABLE_INSTANCING
+        #endif
+
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float3 normal : NORMAL;
+            float4 color : COLOR;
+            float4 tangent : TANGENT;
+            float4 texcoord1 : TEXCOORD1;
+            float4 texcoord2 : TEXCOORD2;
+            uint vid : SV_VertexID;
+            UNITY_VERTEX_INPUT_INSTANCE_ID
+        };
+
+        struct Input
+        {
+            float4 color : COLOR;
+            float vface : VFACE;
+        };
+
+        half4 _Color1;
+        half4 _Color2;
+        half _Smoothness;
+        half _Metallic;
+        float _Scale;
+
+        float4x4 _LocalToWorld;
+        float4x4 _WorldToLocal;
+
+
+        #if defined(ENABLE_INSTANCING)
+
+        StructuredBuffer<float4> _NodeList;
+
+        #endif
+
+        void vert(inout appdata v)
+        {
+            #if defined(ENABLE_INSTANCING)
+
+            uint id = unity_InstanceID;
+            // v.vertex.xyz = _Scale * v.vertex* _NodeList[id].a  + _NodeList[id].xyz  ;
+            v.vertex.xyz = _Scale * v.vertex + _NodeList[id].xyz;
+            
+            v.color = lerp(_Color1, _Color2, 0.5);
+            #endif
+        }
+
+        void setup() 
+        {
+            unity_ObjectToWorld = _LocalToWorld;
+            unity_WorldToObject = _WorldToLocal;
+        }
+
+        void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+            o.Albedo = IN.color;
+            
+            o.Metallic = _Metallic;
+            o.Smoothness = _Smoothness;
+            o.Normal = float3(0, 0, 1);
+        }
+
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
